f7f921f80611367d98af15731e968d1970688fa9
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2026-01-30 09:21:25 +0600

    Time: 11 ms (75.45%) | Memory: 19.4 MB (32.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t): return False #base case
+        cnt = {}
+        for x in s: cnt[x] = cnt.get(x, 0) + 1 #increment 1 with cnt[x]. if cnt[x] is not assign, assign with 0 and +1, else cnt[x] + 1 = old value + 1.
 
-#TC: O(sort algo) = O(Nlogn)
-#SC: O(1)
+        for x in t:
+            if x not in cnt or cnt.get(x) == 0: return False
+            cnt[x] -= 1
+
+        return True
+
+#TC: O(len(str)) = O(n)
+#SC: O(len(cnt)) = O(n)
         
