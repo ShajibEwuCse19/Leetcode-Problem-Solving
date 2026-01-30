7f15a0635a9744971c3bdf998ebb17ff5865b9b9
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2026-01-30 09:41:21 +0600

    Time: 15 ms (42.74%) | Memory: 19.4 MB (40.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,7 +1,20 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        if len(s) != len(t):
+            return False
+        
+        count = [0] * 26
+
+        for char in s:
+            count[ord(char) - ord('a')] += 1
+        
+        for char in t:
+            if count[ord(char) - ord('a')] == 0:
+                return False
+            count[ord(char) - ord('a')] -= 1
+
+        return True
 
 #TC: O(len(str)) = O(n) #alphabet size fixed
-#SC: O(len(cnt)) = O(1)
+#SC: O(len(cnt)) = O(26 - max char) = O(1)
         
