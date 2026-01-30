0400741d2297a02e112bfdd7f9e93a5c6b38ef5b
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2026-01-30 09:23:56 +0600

    Time: 3 ms (98.30%) | Memory: 19.4 MB (32.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,15 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t): return False #base case
-        cnt = {}
-        for x in s: cnt[x] = cnt.get(x, 0) + 1 #increment 1 with cnt[x]. if cnt[x] is not assign, assign with 0 and +1, else cnt[x] + 1 = old value + 1.
+        return Counter(s) == Counter(t)
 
-        for x in t:
-            if x not in cnt or cnt.get(x) == 0: return False
-            cnt[x] -= 1
-
-        return True
-
-#TC: O(len(str)) = O(n)
-#SC: O(len(cnt)) = O(n)
+#TC: O(len(str)) = O(n) #alphabet size fixed
+#SC: O(len(cnt)) = O(1)
         
