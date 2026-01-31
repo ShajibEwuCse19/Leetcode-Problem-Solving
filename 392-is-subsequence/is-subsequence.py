fc1d355981058966fad0225d7871844527180120
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2026-01-31 11:33:53 +0600

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.60%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -1,16 +1,13 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        left = 0
-        for i in range(0, len( s)):
-            find = False
-            for j in range(left, len(t)):
-                if s[i] == t[j]:
-                    find = True
-                    left = j + 1 
-                    break
-            if find == False: return find
-        return True
+        leftS = leftT = 0
+        while leftS < len(s) and leftT < len(t):
+            if s[leftS] == t[leftT]:
+                leftS += 1
+            leftT += 1
+        
+        return leftS == len(s)
 
-#TC: O(len s + len t) = O(n+n) = O(2n) = O(n) #visiting each element only once
+#TC: O(max(len(s,t))) = O(n)
 #SC: O(1) -> no extra space using
             
